2af682795656ae216658c8984222a245df3e6240
athletically <athletically123@gmail.com>
2024-07-11 01:00:33 +0530

    added new api for admin get all users

--- app/controllers/commonController.js
+++ app/controllers/commonController.js
@@ -145,10 +145,12 @@ function getContentType(fileName) {
 }
 
 async function getAllUsers(search, filter, sort) {
-    let match = {
-        name : (search) ? new RegExp(`^${search}`, 'i') : undefined,
-        game : (filter) ? filter : undefined
-    }
+    let match = {}
+    if(search)
+        match.name = new RegExp(`^${search}`, 'i');
+    if(filter)
+        game = filter
+    
     console.log(match);
     let AllUsers = await UserModel.find(match, '-password').lean();
 
